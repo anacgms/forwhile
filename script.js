06bc935b1919e3c930522e90e39937a477b9d985
anacgms <acgomes43@gmail.com>
2022-07-05 22:53:52 -0300

    forwhile

---
+++ script.js
@@ -0,0 +1,20 @@
+for(let num = 1; num <= 10; num++){
+    console.log(num)
+ }
+
+ let contagem = 10
+
+ while(contagem >= 1){
+     console.log(contagem--)
+ }
+
+ for(let i = 1; i <= 100; i +=2){
+     console.log(i)
+ }
+
+ let number = 0
+
+ while(number <= 100){
+     console.log(number)
+     number += 2
+ }
